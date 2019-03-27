759c8bc07303b3a46c0e8334f912475d199c336a
Graeme Lockley <graemel@no9.co.za>
2019-03-27 05:50:37 +0200

    chore: integrate kotlin into the top level POM

---
+++ domain/src/main/kotlin/za.co.no9.mes8.domain/Services.kt
@@ -0,0 +1,6 @@
+package za.co.no9.mes8.domain
+
+
+fun main(args: Array<String>) {
+    println("Hello World")
+}
---
+++ domain/src/test/kotlin/za/co/no9/mes8/domain/ServicesTest.kt
@@ -0,0 +1,10 @@
+package za.co.no9.mes8.domain
+
+import io.kotlintest.specs.StringSpec
+
+
+class ServicesTest : StringSpec( {
+    "some test" {
+
+    }
+})
